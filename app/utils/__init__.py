69d80c848cfa03696a019689ae2318668bc9b8e1
nrkimv6 <risingnrkim@outlook.kr>
2025-12-01 16:40:57 +0900

    Implement asynchronous logging and database writing: integrate AsyncLoggerManager and AsyncDBWriter for non-blocking operations in the worker process

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -2,6 +2,8 @@
 
 from .validators import is_naver_full_reservation, is_naver_page_available, is_naver_content_valid
 from .parsers import extract_date_from_url, calculate_interval, parse_time_and_stock, parse_naver_page_info
+from .async_db_writer import AsyncDBWriter, get_db_writer, shutdown_db_writer
+from .async_logger import AsyncLoggerManager, setup_async_logging
 
 __all__ = [
     'is_naver_full_reservation',
@@ -10,5 +12,11 @@ __all__ = [
     'extract_date_from_url',
     'calculate_interval',
     'parse_time_and_stock',
-    'parse_naver_page_info'
+    'parse_naver_page_info',
+    # 비동기 IO
+    'AsyncDBWriter',
+    'get_db_writer',
+    'shutdown_db_writer',
+    'AsyncLoggerManager',
+    'setup_async_logging',
 ] 
